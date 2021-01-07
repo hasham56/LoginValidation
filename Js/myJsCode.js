1b867d9c7b56114717b106e6f495ac803e3bcd64
hasham56 <hashammeher@gmail.com>
2021-01-08 01:00:05 +0500

    final changes

--- Js/myJsCode.js
+++ Js/myJsCode.js
@@ -1,9 +1,13 @@
-$(function () {
-    $("#myForm").submit(handleFormSubmit);
-  });
-  
-  function handleFormSubmit(e) {
-    e.preventDefault(); //will stop form event
-    $("#exampleInputEmail1").addClass("invalid");
-    //   alert("Handle Your code");
+function Submit(){
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  if (email == "example@google.com" && password == "123456"){
+    document.body.style.backgroundColor = "green";
+    document.getElementById("identification").innerHTML = "Login was a success.";
   }
+  else{
+    document.body.style.backgroundColor = "red";
+    document.getElementById("identification").innerHTML = "Invalid Credentials."
+  }
+}
